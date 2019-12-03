224b5344ecd51cf3feb81a8d9b8ae76a1e8ddae1
William Sinn <w.h.sinn@gmail.com>
2019-12-03 00:06:47 -0500

    add home-icon, add hover navbar styles, add trello navbar color scheme, add hover animations respectively

--- src/components/HomeBtn.js
+++ src/components/HomeBtn.js
@@ -3,11 +3,17 @@ import { connect } from "react-redux";
 import { clearWorkspace } from "../actions/workspace";
 
 const HomeBtn = (props, { dispatch }) => (
-  <button
-    className="navbar-btn"
+  <a
+    className="navbar-btn home"
     onClick={e => props.dispatch(clearWorkspace(e))}
   >
-    Home
-  </button>
+    <img
+      className="home-icon"
+      src={require("../images/home-icon.png")}
+      alt="home"
+      backgroundColor="inherit"
+      opacity="1"
+    />
+  </a>
 );
 export default connect()(HomeBtn);
