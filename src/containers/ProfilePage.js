046a306405ec289f8229edb8236c4de87920faf3
William Sinn <w.h.sinn@gmail.com>
2019-12-15 07:19:09 -0500

    add user-profile navbar

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -2,17 +2,59 @@ import React from "react";
 
 const ProfilePage = () => {
   return (
-    <div className="profile-page">
-      <div className="tabbed-profile-header">
-        <div className="tabbed-profile-header wrapper">
-          <div className="profile-nicknames">
-            <span className="user-initials">WS</span>
-            <span className="user-fullname">William Sinn</span>
-            <span className="user-handle">@WilliamSinn1</span>
+    <>
+      <div className="profile-page">
+        <div className="tabbed-profile-header">
+          <div className="tabbed-profile-header wrapper">
+            <div className="profile-nicknames">
+              <span className="user-initials">WS</span>
+              <span className="user-fullname">William Sinn</span>
+              <span className="user-handle">@WilliamSinn1</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div class="tabbed-pane-nav">
+        <ul>
+          <li class="tabbed-pane-nav-item">
+            <a
+              class="tabbed-pane-nav-item-button js-member-profile"
+              data-tab="profile"
+              href="/williamsinn1"
+            >
+              Profile and Visibility
+            </a>
+          </li>
+          <li class="tabbed-pane-nav-item">
+            <a
+              class="tabbed-pane-nav-item-button js-member-activity active"
+              data-tab="cards"
+              href="/williamsinn1/activity"
+            >
+              Activity
+            </a>
+          </li>
+          <li class="tabbed-pane-nav-item">
+            <a
+              class="tabbed-pane-nav-item-button js-member-cards"
+              data-tab="cards"
+              href="/williamsinn1/cards"
+            >
+              Cards
+            </a>
+          </li>
+          <li class="tabbed-pane-nav-item">
+            <a
+              class="tabbed-pane-nav-item-button js-member-account"
+              data-tab="settings"
+              href="/williamsinn1/account"
+            >
+              Settings
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
