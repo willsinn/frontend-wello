a7f2f2293c186596228bb5d8caae8b2f9d8ee34c
William Sinn <w.h.sinn@gmail.com>
2020-02-22 09:56:40 -0500

    change background style to inline, add local state for modal background

--- src/components/AddBoardModal.js
+++ src/components/AddBoardModal.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import NewBoardForm from "./NewBoardForm";
 
 const AddBoardModal = props => {
+  const [background, setBackground] = useState("lightblue");
   return (
     <div
       className="add-board-modal"
       style={props.active ? { display: "block" } : { display: "none" }}
     >
-      <div className="modal-content" onClick={null}>
+      <div className="modal-content" style={{ background: `${background}` }}>
         <NewBoardForm active={props.active} closeModal={props.closeModal} />
         <button className="modal close-btn" onClick={e => props.closeModal(e)}>
           <span>x</span>
