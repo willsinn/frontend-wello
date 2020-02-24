59b3c8677d0851d8eb14f826e1f2b7c8b86265a8
William Sinn <w.h.sinn@gmail.com>
2020-02-23 22:16:42 -0500

    render bg color to tiles

--- src/components/BoardTiles.js
+++ src/components/BoardTiles.js
@@ -6,7 +6,11 @@ const BoardTiles = props => {
   const renderTiles = () => {
     if (props.boards.length > 0) {
       return props.boards.map(board => (
-        <li key={board.id} className="board-tile">
+        <li
+          key={board.id}
+          className="board-tile"
+          style={{ background: `${board.background}` }}
+        >
           <div className="board-title">
             <ProjectListItem key={board.id} board={board} />
           </div>
