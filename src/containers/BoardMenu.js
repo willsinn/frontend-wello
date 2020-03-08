d8adbe19b49cde7090bfba49def3cfef2483cf82
William Sinn <w.h.sinn@gmail.com>
2020-03-08 12:02:44 -0400

    find workaround on rendering sidebar; toggled transform on local state

--- src/containers/BoardMenu.js
+++ src/containers/BoardMenu.js
@@ -3,15 +3,22 @@ import DelBoardBtn from "../components/DelBoardBtn";
 
 const BoardMenu = props => {
   const [sidebar, setSidebar] = useState(false);
+  const renderSidebar = () => {
+    return !sidebar ? { transform: "translateX(339px)" } : null;
+  };
   return (
     <div className="board-sidebar">
-      <button className="show-menu-btn board-header-btn">
+      <button
+        onClick={e => setSidebar(true)}
+        className="show-menu-btn board-header-btn"
+      >
         <span className="icon-sm">...</span>
         <span>Show Menu</span>
       </button>
-      <div className="board-menu">
+      <div className="board-menu" style={renderSidebar()}>
         <div className="board-menu-container">
           <DelBoardBtn workspace={props.workspace} />
+          <button onClick={e => setSidebar(false)}>x</button>
         </div>
       </div>
     </div>
