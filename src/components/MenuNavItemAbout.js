de19609acba31d4d0afca9de08f3698185cd7cdc
William Sinn <w.h.sinn@gmail.com>
2020-03-13 18:07:07 -0400

    add html frame, need desc fn

--- src/components/MenuNavItemAbout.js
+++ src/components/MenuNavItemAbout.js
@@ -4,7 +4,7 @@ const MenuNavItemAbout = props => {
   return (
     <div className="board-menu-container">
       <div className="board-menu-sidebar-content">
-        <div class="board-menu-header-content">
+        <div className="board-menu-header-content">
           <button onClick={e => props.setContent("")}>〈</button>
           <h3 class="board-menu-header-title js-board-menu-title-text">
             About This Board
@@ -17,6 +17,24 @@ const MenuNavItemAbout = props => {
           </button>
         </div>
         <hr />
+        <div>
+          <div>
+            <h2>
+              <span>Profile Icon</span>
+              <span>Made By</span>
+            </h2>
+          </div>
+          <div> Board Creator </div>
+        </div>
+        <div>
+          <div>
+            <h2>
+              <span>Desc Icon</span>
+              <span>Description</span>
+            </h2>
+          </div>
+          <div> Description Box </div>
+        </div>
       </div>
     </div>
   );
