348b1098971a63a57ad7019d9386985e4832a2ae
William Sinn <w.h.sinn@gmail.com>
2020-03-17 13:03:44 -0400

    add template for menu style to last two buttons

--- src/components/MenuNavItemSearch.js
+++ src/components/MenuNavItemSearch.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const MenuNavItemSearch = props => {
-  return <div>Search Cards</div>;
+  return (
+    <div className="board-menu-container">
+      <div className="board-menu-sidebar-content">
+        <div className="board-menu-header-content">
+          <button
+            className="board-menu-back-btn"
+            onClick={e => props.setContent("")}
+          >
+            <span className="back-text">←</span>
+          </button>
+          <h3 className="board-menu-header-title js-board-menu-title-text">
+            Search any Card
+          </h3>
+          <button
+            className="board-menu-close-btn"
+            onClick={e => props.setSidebar(false)}
+          >
+            <span className="close-text">✕</span>
+          </button>
+        </div>
+        <hr />
+        <div className="board-menu-content">
+          <div className="">Add Search Bar Eventually</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuNavItemSearch;
--- src/containers/MenuNavItemMore.js
+++ src/containers/MenuNavItemMore.js
@@ -2,12 +2,31 @@ import React from "react";
 
 const MenuNavItemMore = props => {
   return (
-    <div>
-      <h1>Menu Nav Item</h1>
-      <ul>
-        <li>Share Link</li>
-        <li>Delete Board</li>
-      </ul>
+    <div className="board-menu-container">
+      <div className="board-menu-sidebar-content">
+        <div className="board-menu-header-content">
+          <button
+            className="board-menu-back-btn"
+            onClick={e => props.setContent("")}
+          >
+            <span className="back-text">←</span>
+          </button>
+          <h3 className="board-menu-header-title js-board-menu-title-text">
+            More Options
+          </h3>
+          <button
+            className="board-menu-close-btn"
+            onClick={e => props.setSidebar(false)}
+          >
+            <span className="close-text">✕</span>
+          </button>
+        </div>
+        <hr />
+        <div className="board-menu-content">
+          <div className="">Delete Board</div>
+          <div className="">Link to Friend</div>
+        </div>
+      </div>
     </div>
   );
 };
