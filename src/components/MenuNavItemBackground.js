a2550b53186e27450b708cd3ff7deb792f9051c0
William Sinn <w.h.sinn@gmail.com>
2020-03-17 12:58:23 -0400

    rewrite change background menu item

--- src/components/MenuNavItemBackground.js
+++ src/components/MenuNavItemBackground.js
@@ -1,7 +1,37 @@
 import React from "react";
+import BgList from "./BgList";
 
 const MenuNavItemBackground = props => {
-  return <div>Change Background</div>;
+  return (
+    <div className="board-menu-container">
+      <div className="board-menu-sidebar-content">
+        <div className="board-menu-header-content">
+          <button
+            className="board-menu-back-btn"
+            onClick={e => props.setContent("")}
+          >
+            <span className="back-text">←</span>
+          </button>
+          <h3 className="board-menu-header-title js-board-menu-title-text">
+            About This Board
+          </h3>
+          <button
+            className="board-menu-close-btn"
+            onClick={e => props.setSidebar(false)}
+          >
+            <span className="close-text">✕</span>
+          </button>
+        </div>
+        <hr />
+        <div className="board-menu-content">
+          <div className="menu-background-tiles-container">
+            <div className="">Photos</div>
+            <div className="">Colors</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuNavItemBackground;
