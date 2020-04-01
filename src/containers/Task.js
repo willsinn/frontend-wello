b20f3264aea55c07cf59398bb05cb7068da54d21
William Sinn <w.h.sinn@gmail.com>
2020-04-01 19:43:07 -0400

    fix edit task btn, add show functionality - app renders and works

--- src/containers/Task.js
+++ src/containers/Task.js
@@ -11,12 +11,14 @@ const Task = props => {
     >
       <div className="task-item-note">
         <span>{props.task.note}</span>
-        <span
-          className="edit-task-item-btn"
-          onClick={e => props.renderQuickEditor(e, props.task.note)}
-        >
-          edit
-        </span>
+        {visible ? (
+          <span
+            className="edit-task-item-btn"
+            onClick={e => props.renderQuickEditor(e, props.task.note)}
+          >
+            ✎
+          </span>
+        ) : null}
       </div>
       <div>
         <span>Desc</span>
