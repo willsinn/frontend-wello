c16756a628bf42c1d102300dfb98098803c1b6ce
William Sinn <w.h.sinn@gmail.com>
2020-04-20 15:48:50 -0400

    move onClick, may need a render function or increase element size

--- src/containers/Task.js
+++ src/containers/Task.js
@@ -3,16 +3,16 @@ import React, { useState } from "react";
 const Task = ({ task, card, renderQuickEditor }) => {
   const [visible, setVisible] = useState(false);
   const [window, setWindow] = useState(false);
+  const handleRenderQuickEditor = () => {};
   return (
     <div
       className="task-item task-item-details"
-      onClick={(e) => setWindow(true)}
       onMouseEnter={(e) => setVisible(true)}
       onMouseLeave={(e) => setVisible(false)}
     >
       {window ? <div className="task-window">Hi Im task Window</div> : null}
       <div className="task-item-note">
-        <span>{task.note}</span>
+        <span onClick={(e) => setWindow(true)}>{task.note}</span>
         {visible ? (
           <span
             className="edit-task-item-btn"
