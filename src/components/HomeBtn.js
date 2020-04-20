d40e1a44689661be076e09b75b7fc385d755e263
William Sinn <w.h.sinn@gmail.com>
2020-04-20 14:39:55 -0400

    fix beautify issue

--- src/components/HomeBtn.js
+++ src/components/HomeBtn.js
@@ -1,27 +1,18 @@
 import React from "react";
-import {
-  connect
-} from "react-redux";
-import {
-  setPage
-} from "../actions/user";
+import { connect } from "react-redux";
+import { setPage } from "../actions/user";
 
-const HomeBtn = (props, {
-  dispatch
-}) => ( <
-  button className = "navbar-btn"
-  onClick = {
-    e => props.dispatch(setPage("home"))
-  } >
-  <
-  img className = "icon"
-  src = {
-    require("../images/home-icon.png")
-  }
-  alt = "home"
-  opacity = "1" /
+const HomeBtn = (props, { dispatch }) => (
+  <button
+    className="navbar-btn"
+    onClick={(e) => props.dispatch(setPage("home"))}
   >
-  <
-  /button>
+    <img
+      className="icon"
+      src={require("../images/home-icon.png")}
+      alt="home"
+      opacity="1"
+    />
+  </button>
 );
 export default connect()(HomeBtn);
