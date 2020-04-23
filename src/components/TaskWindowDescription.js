1b1c478b0a1c721b147843225c3e5ca3120c720b
William Sinn <w.h.sinn@gmail.com>
2020-04-23 15:12:59 -0400

    add conditional managing edit description, works and renders as intended. verified using logs

--- src/components/TaskWindowDescription.js
+++ src/components/TaskWindowDescription.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 
 const TaskWindowDescription = ({ taskDesc }) => {
-  console.log(taskDesc);
+  const [desc, setDesc] = useState({});
+  console.log(desc);
+
   return (
     <div className="task-window-desc">
       <div className="module-header">
@@ -11,11 +13,20 @@ const TaskWindowDescription = ({ taskDesc }) => {
           <button className="edit-desc-btn">Edit</button>
         </div>
       </div>
-      <div className="module-body">
-        <div className="desc-placeholder">
-          <p>Add a more detailed description...</p>
+
+      {desc ? (
+        <div className="module-body" onClick={(e) => setDesc("")}>
+          {!taskDesc ? (
+            <div className="desc-placeholder">
+              <p>Add a more detailed description...</p>
+            </div>
+          ) : (
+            { taskDesc }
+          )}
         </div>
-      </div>
+      ) : (
+        console.log("exists")
+      )}
     </div>
   );
 };
