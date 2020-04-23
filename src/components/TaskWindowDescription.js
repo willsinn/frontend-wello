912bf63b1334f425db2b39f6e8625584790f5312
William Sinn <w.h.sinn@gmail.com>
2020-04-22 21:09:56 -0400

    add edit-desc-btn styles, positioned, spaced, hover animations

--- src/components/TaskWindowDescription.js
+++ src/components/TaskWindowDescription.js
@@ -6,7 +6,9 @@ const TaskWindowDescription = (props) => {
     <div className="task-window-desc">
       <div className="desc-header">
         <h3>Description</h3>
-        <div className="editable-desc">edit</div>
+        <div className="editable-desc">
+          <button className="edit-desc-btn">Edit</button>
+        </div>
       </div>
     </div>
   );
