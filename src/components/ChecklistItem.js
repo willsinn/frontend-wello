a9d5837becde95073d6f6483af5c8a53cd1a29bc
William Sinn <w.h.sinn@gmail.com>
2020-04-25 16:33:40 -0400

    add html elems to frame feature renders"

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -3,7 +3,25 @@ import React from "react";
 const ChecklistItem = ({ item }) => {
   console.log("c", item);
 
-  return <div className="checklist-item">{item.item}</div>;
+  return (
+    <div className="checklist-item">
+      <div className="checklist-item-check-box"></div>
+      <div className="checklist-item-details">
+        <div className="checklist-item-row">
+          <div className="checklist-item-text-and-controls checklist-item-details">
+            <span className="checklist-item-details-text">{item.item}</span>
+            <div class="checklist-item-controls">
+              <div class="checklist-item-menu js-open-menu">
+                <span class="checklist-icon-sm icon-sm icon-overflow-menu-horizontal">
+                  ...
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChecklistItem;
