92fa6c235a2519f9f0aa3d7eaddd08745d524d86
William Sinn <w.h.sinn@gmail.com>
2020-04-25 19:28:07 -0400

    rewrite & layer elements differently to space & position the menu thats only visible during activeHover

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,22 +1,29 @@
 import React from "react";
 
-const ChecklistItem = ({ item }) => {
-  console.log("c", item);
+const ChecklistItem = ({ item, hovering, handleHoveringItem }) => {
+  console.log("c", item, hovering);
 
   return (
-    <div className="checklist-item">
+    <div
+      className="checklist-item"
+      onMouseEnter={(e) => handleHoveringItem(e, item)}
+    >
       <div className="checklist-item-check-box"></div>
       <div className="checklist-item-details">
         <div className="checklist-item-row">
           <div className="checklist-item-text-and-controls checklist-item-details">
             <span className="checklist-item-details-text">{item.item}</span>
-            <div class="checklist-item-controls">
-              <div class="checklist-item-menu js-open-menu">
-                <span class="checklist-icon-sm icon-sm icon-overflow-menu-horizontal">
-                  ...
-                </span>
+          </div>
+          <div>
+            {hovering.id === item.id ? (
+              <div class="checklist-item-controls">
+                <div class="checklist-item-menu js-open-menu">
+                  <span class="checklist-icon-sm icon-sm icon-overflow-menu-horizontal">
+                    ...
+                  </span>
+                </div>
               </div>
-            </div>
+            ) : null}
           </div>
         </div>
       </div>
--- src/components/ChecklistItemList.js
+++ src/components/ChecklistItemList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ChecklistItem from "./ChecklistItem";
 
 const ChecklistItemList = ({ items }) => {
+  const [hovering, setHovering] = useState({ id: undefined });
+  const handleHoveringItem = (e, tarItem) => {
+    setHovering({ id: tarItem.id });
+  };
   const renderChecklistItems = () => {
     if (items && items.length > 0) {
       return items.map((item) => {
-        return <ChecklistItem key={item.id} item={item} />;
+        return (
+          <ChecklistItem
+            key={item.id}
+            item={item}
+            hovering={hovering}
+            handleHoveringItem={handleHoveringItem}
+          />
+        );
       });
     }
   };
@@ -15,7 +26,12 @@ const ChecklistItemList = ({ items }) => {
         <span className="checklist-progress-percentage"></span>
         <div className="checklist-progress-bar"></div>
       </div>
-      <div className="checklist-items-list">{renderChecklistItems()}</div>
+      <div
+        className="checklist-items-list"
+        onMouseLeave={(e) => setHovering({ id: undefined })}
+      >
+        {renderChecklistItems()}
+      </div>
     </div>
   );
 };
