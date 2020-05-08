ae9920a91966fa800c23e677f5f23ad97ba9c2a1
William Sinn <w.h.sinn@gmail.com>
2020-05-08 19:30:28 -0400

    remove excess code, remove console.logs

--- src/components/LabelsList.js
+++ src/components/LabelsList.js
@@ -2,65 +2,12 @@ import React from "react";
 import Label from "../containers/Label";
 
 const LabelsList = ({ labels }) => {
-  console.log("LABELS_LIST", labels);
   const renderLabels = () => {
     return labels.map((label) => {
       return <Label key={label.id} label={label} />;
     });
   };
-  return (
-    <ul className="label-popover-list">
-      {renderLabels()}
-      <li className="label-popover-list-item">
-        <span className="task-label-green task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-      <li className="label-popover-list-item">
-        <span className="task-label-yellow task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-      <li className="label-popover-list-item">
-        <span className="task-label-orange task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-      <li className="label-popover-list-item">
-        <span className="task-label-red task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-      <li className="label-popover-list-item">
-        <span className="task-label-purple task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-      <li className="label-popover-list-item">
-        <span className="task-label-blue task-label">
-          <p>text</p>
-        </span>
-        <div className="name-label-btn-container">
-          <button className="name-label-btn">✐</button>
-        </div>
-      </li>
-    </ul>
-  );
+  return <ul className="label-popover-list">{renderLabels()}</ul>;
 };
 
 export default LabelsList;
