d4e767d483ed6677cb0cbaf4e18a2529a79e65fd
William Sinn <w.h.sinn@gmail.com>
2020-05-11 17:05:05 -0400

    add complete task label styles, renders as intended

--- src/components/TaskLabel.js
+++ src/components/TaskLabel.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TaskLabel = ({ label }) => {
-  return <div>111</div>;
+  return (
+    <li className="task-labels-list-item">
+      <span className={`tlabel-item tlabel-${label.color}`}>{label.name}</span>
+    </li>
+  );
 };
 
 export default TaskLabel;
