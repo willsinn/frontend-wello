e98efae840cfc9928faa99e1fc9ca5df18ef0c4a
William Sinn <w.h.sinn@gmail.com>
2020-05-19 10:49:12 -0400

    add LoginForm into components, remove state.name & clean up respectively

---
+++ src/components/LoginForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { userSignup } from "../actions/user";
+
+const LoginForm = ({ dispatch }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const resetState = () => {
+    setEmail("");
+    setPassword("");
+  };
+  const handleEmailChange = (e) => {
+    e.persist();
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    e.persist();
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    if (e) {
+      e.preventDefault();
+      dispatch(userSignup({ email, password }));
+      resetState();
+    }
+  };
+
+  return (
+    <div className="card-item-wrap">
+      <div className="card-item">
+        <form>
+          <input
+            className="add-card-input"
+            type="text"
+            name="email"
+            onChange={handleEmailChange}
+            value={email}
+            placeholder="Enter email"
+          />
+          <input
+            className="add-card-input"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={password}
+            placeholder="Create password"
+          />
+          <button onClick={handleSubmit} className="add-list-btn" type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default connect()(LoginForm);
