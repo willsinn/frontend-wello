38f3d3d4b45295b3daa8e651e097d7af3e145548
William Sinn <w.h.sinn@gmail.com>
2020-06-02 14:02:17 -0400

    add archived container, renders to homepage

---
+++ src/containers/Archived.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Archived = (props) => {
+  return <div>Hello this is archived</div>;
+};
+
+export default Archived;
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PersonalBoardList from "../components/PersonalBoardList";
+import Archived from "./Archived";
 
-const HomePage = props => {
+const HomePage = (props) => {
   return (
     <div className="home-page">
-      <h2>Starred</h2>
-      <PersonalBoardList />
-      <h2>Team Boards</h2>
+      <div>
+        <h2>Starred</h2>
+        <PersonalBoardList />
+        <h2>Team Boards</h2>
+      </div>
+
+      <div>
+        <Archived />
+      </div>
     </div>
   );
 };
