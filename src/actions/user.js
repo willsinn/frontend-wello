ee5576649928bcaa44f6a5af7012e2fe634e402d
William Sinn <w.h.sinn@gmail.com>
2020-06-13 17:43:40 -0400

    add LoginForm, checked input updates and hits debugger in handleSubmit

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,19 +1,19 @@
-export const setUser = userData => ({
+export const setUser = (userData) => ({
   type: "SET_USER",
-  payload: userData
+  payload: userData,
 });
-export const setPage = page => ({
+export const setPage = (page) => ({
   type: "SET_PAGE",
-  page
+  page,
 });
-export const fetchUser = data => {
-  return dispatch => {
+export const fetchUser = (data) => {
+  return (dispatch) => {
     fetch("http://localhost:3000/user/1", {
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
     // .then(response => {
     //   if (response.ok) {
@@ -23,5 +23,31 @@ export const fetchUser = data => {
     //   }
     // });
     // .then(JSONresponse => console.log(JSONresponse));
+  };
+};
+
+export const userLogin = (username, password) => {
+  debugger;
+  return (dispatch) => {
+    dispatch({ type: "AUTHENTICATING_USER" });
+    fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw response;
+      }
+    });
   };
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,56 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { userLogin } from "../actions/user";
 
 const LoginForm = ({}) => {
-  return <div>Hi this is the login form</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUnChange = (e) => {
+    e.persist();
+    setUsername(e.target.value);
+  };
+  const handlePwChange = (e) => {
+    e.persist();
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    if (e) {
+      debugger;
+      e.preventDefault();
+      //   dispatch(postNewChecklist(task, title));
+      //   setTitle("Checklist");
+      //   closePopup();
+    }
+  };
+  console.log(username, password);
+
+  return (
+    <div>
+      Hi this is the login form
+      <form onSubmit={handleSubmit}>
+        <input
+          className="add-checklist-input"
+          type="text"
+          name="username"
+          onChange={handleUnChange}
+          value={username}
+          required
+        />
+        <input
+          className="add-checklist-input"
+          type="text"
+          name="password"
+          onChange={handlePwChange}
+          value={password}
+          required
+        />
+        <button className="checklist-btn add-list-btn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default LoginForm;
+export default connect()(LoginForm);
