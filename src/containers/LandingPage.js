12637deb159022a1516fc3814e08aa714b00cfac
William Sinn <w.h.sinn@gmail.com>
2020-06-17 16:27:48 -0400

    create LandingPage, import LoginForm

---
+++ src/containers/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <LoginForm />
+      This is the landing page
+    </div>
+  );
+};
+
+export default LandingPage;
