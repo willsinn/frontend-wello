c7a5fcd2b78911f4a6104fd6b460e0d64c4c15df
William Sinn <w.h.sinn@gmail.com>
2020-06-18 12:14:54 -0400

    create branch, reformat code with boards & archives buttons

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import withAuth from "../hocs/withAuth";
 import PersonalBoardList from "../components/PersonalBoardList";
-
+import Archives from "./Archives";
 import NavBar from "./NavBar";
 import { connect } from "react-redux";
 import { fetchUserBoards } from "../actions/boards";
 import { Redirect } from "react-router-dom";
 
 const HomePage = ({ user, workspace, dispatch }) => {
+  const [boards, setBoards] = useState(true);
   const renderBoards = () => {
     dispatch(fetchUserBoards(user.id));
   };
@@ -19,10 +20,23 @@ const HomePage = ({ user, workspace, dispatch }) => {
         <Redirect to="/board" />
       ) : (
         <div className="home-page">
-          <h2>Starred</h2>
-          {renderBoards()}
-          <PersonalBoardList />
-          <h2>Team Boards</h2>
+          <div style={{ position: "absolute", top: "40px", left: "0px" }}>
+            <div className="archive-boards-menu">
+              <button onClick={(e) => setBoards(true)}>Boards</button>
+              <button onClick={(e) => setBoards(false)}>Archives</button>
+            </div>
+          </div>
+
+          {boards ? (
+            <div className="boards-container">
+              <h2>Starred</h2>
+              {renderBoards()}
+              <PersonalBoardList />
+              <h2>Team Boards</h2>
+            </div>
+          ) : (
+            <Archives />
+          )}
         </div>
       )}
     </div>
