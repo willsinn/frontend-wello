5ada75bf652385fae22e4a9ca522511dd017b8bb
William Sinn <w.h.sinn@gmail.com>
2020-06-18 16:24:18 -0400

    save working fetch to archived endpoint

--- src/actions/archives.js
+++ src/actions/archives.js
@@ -8,13 +8,18 @@ export const setArchives = (archives) => ({
 });
 
 export const fetchAllArchived = (user) => {
+  debugger;
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/user/${user.user_id}/archived`, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
         Authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
+      body: JSON.stringify({
+        user_id: `${user.user_id}`,
+      }),
     })
       .then((response) => response.json())
       .then((JSONresponse) => dispatch(setArchives(JSONresponse)));
