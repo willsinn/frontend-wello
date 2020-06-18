f85dee9215761a28b35364c4306c4b55c43bdd52
William Sinn <w.h.sinn@gmail.com>
2020-06-18 02:08:57 -0400

    fix Redirect problem for homeBtn, fix clearWorkspace at workspaceReducer: app renders and runs smoothly

--- src/components/HomeBtn.js
+++ src/components/HomeBtn.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setPage } from "../actions/user";
+import { clearWorkspace } from "../actions/workspace";
 
-const HomeBtn = (props, { dispatch }) => (
-  <button
-    className="navbar-btn"
-    onClick={(e) => props.dispatch(setPage("home"))}
-  >
-    <img
-      className="icon"
-      src={require("../images/home-icon.png")}
-      alt="home"
-      opacity="1"
-    />
-  </button>
-);
+const HomeBtn = ({ dispatch }) => {
+  return (
+    <button className="navbar-btn" onClick={(e) => dispatch(clearWorkspace())}>
+      <img
+        className="icon"
+        src={require("../images/home-icon.png")}
+        alt="home"
+        opacity="1"
+      />
+    </button>
+  );
+};
 export default connect()(HomeBtn);
--- src/reducers/workspaceReducer.js
+++ src/reducers/workspaceReducer.js
@@ -7,7 +7,7 @@ const workspaceReducer = (state = defaultState, action) => {
     case "TOGGLE_EDIT":
       return { ...state, isEditActive: !state.isEditActive };
     case "CLEAR_WORKSPACE":
-      return { ...state, workspace: defaultState, cards: [] };
+      return { ...state, workspace: {} };
     case "SET_WORKSPACE":
       return { ...state, workspace: action.workspace.board };
     case "ADD_CARD":
