26bb4d8ccac1872f5207752ed8836cad85b849b2
William Sinn <w.h.sinn@gmail.com>
2020-06-21 19:11:40 -0400

    add button styles, refactor buttons to be inside conditional

--- src/components/BoardArchiveItem.js
+++ src/components/BoardArchiveItem.js
@@ -15,47 +15,54 @@ const BoardArchiveItem = ({ boardArchive }) => {
     }
   };
   return (
-    <li className="archives-list-item">
-      <div className="archive-item-content straight-row-content">
-        <div className="straight-row-content a-left">
-          <span className="archive-item-info">Board</span>
-          <span className="archive-item-info archive-title-text">{`${boardArchive.title}`}</span>
+    <>
+      <li className="archives-list-item">
+        <div className="archive-item-content straight-row-content">
           <div className="straight-row-content a-left">
-            {boardArchive.cards.length > 0 ? (
-              <div>
-                <button
-                  className="action-dropdown-btn"
-                  onClick={(e) => setRender(!render)}
-                >
+            <span className="archive-item-info">Board</span>
+            <span className="archive-item-info archive-title-text">{`${boardArchive.title}`}</span>
+            <div className="straight-row-content a-left">
+              {boardArchive.cards.length > 0 ? (
+                <div>
                   {render ? (
-                    <span className="open-info-btn">⌃</span>
+                    <button
+                      className="closed-info-btn"
+                      onClick={(e) => setRender(!render)}
+                    >
+                      <span className="action-dropdown-icon">⌃</span>
+                    </button>
                   ) : (
-                    <span className="open-info-btn">hihi</span>
+                    <button
+                      className="open-info-btn"
+                      onClick={(e) => setRender(!render)}
+                    >
+                      <span className="action-dropdown-icon">⌃</span>
+                    </button>
                   )}
-                </button>
-                <span className="archive-item-info">cards</span>
-              </div>
-            ) : null}
+                  <span className="archive-item-info">cards</span>
+                </div>
+              ) : null}
+            </div>
+          </div>
+          <div className="straight-row-content a-right">
+            <span>Archived: </span>
+            <span className="archive-title-info">
+              {boardArchive.date_archived}
+            </span>
+            <div className="restore-archived">
+              <button>
+                <span className="restore-btn-text">restore</span>
+              </button>
+            </div>
           </div>
         </div>
-        <div className="straight-row-content a-right">
-          <span>Archived: </span>
-          <span className="archive-title-info">
-            {boardArchive.date_archived}
-          </span>
-          <div className="restore-archived">
-            <button>
-              <span className="restore-btn-text">restore</span>
-            </button>
-          </div>
+      </li>
+      {render ? (
+        <div className="archive-item-nested-list">
+          <ul className="archive-list">{renderBoardCards()}</ul>
         </div>
-        {render ? (
-          <div className="archive-item-nested-list">
-            <ul className="archive-list">{renderBoardCards()}</ul>
-          </div>
-        ) : null}
-      </div>
-    </li>
+      ) : null}
+    </>
   );
 };
 export default BoardArchiveItem;
