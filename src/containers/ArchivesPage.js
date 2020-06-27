6add205ad10b7f3fac604b203b8ceb007e93c84a
William Sinn <w.h.sinn@gmail.com>
2020-06-26 21:51:24 -0400

    add icons, size and fit to space

--- src/containers/ArchivesPage.js
+++ src/containers/ArchivesPage.js
@@ -6,6 +6,7 @@ const ArchivesPage = () => {
   return (
     <div className="archives-page">
       <div className="board-tiles-section-header">
+        <div className="archive-icon-black" />
         <span className="home-section-title">Archives</span>
       </div>
       <ArchivesList />
