5cb2c179d28511a1a1dc16f0aec5571a05e9bd2d
William Sinn <w.h.sinn@gmail.com>
2020-06-30 10:54:11 -0400

    fix user-icon & name in about board section

--- src/components/MenuNavItemAbout.js
+++ src/components/MenuNavItemAbout.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const MenuNavItemAbout = props => {
+const MenuNavItemAbout = ({ user, setContent, setSidebar }) => {
+  const nameSplit = user.name.split(" ");
+  const renderInitials = () => {
+    let initials = "";
+    const nameSplit = user.name.split(" ");
+    if (nameSplit) {
+      nameSplit.forEach((letter) => {
+        const first = letter[0].toUpperCase();
+        initials += first;
+      });
+    }
+    if (initials.length === 1) {
+      initials += initials;
+    }
+    if (initials.length > 2) {
+      const limit = initials.slice(0, 1);
+      initials = limit;
+    }
+    return initials;
+  };
+
   return (
     <div className="board-menu-container">
       <div className="board-menu-sidebar-content">
         <div className="board-menu-header-content">
           <button
             className="board-menu-back-btn"
-            onClick={e => props.setContent("")}
+            onClick={(e) => setContent("")}
           >
             <span className="back-text">←</span>
           </button>
@@ -16,7 +37,7 @@ const MenuNavItemAbout = props => {
           </h3>
           <button
             className="board-menu-close-btn"
-            onClick={e => props.setSidebar(false)}
+            onClick={(e) => setSidebar(false)}
           >
             <span className="close-text">✕</span>
           </button>
@@ -36,15 +57,15 @@ const MenuNavItemAbout = props => {
                 <div className="about-user-initials">
                   <span className="menu-icon board-menu-nav-btn activity-item">
                     <span className="initials-wrap menu-icon menu-initials">
-                      WS
+                      {renderInitials()}
                     </span>
-                    <div> William Sinn </div>
-                    <div> @William_Sinn_1 </div>
+                    <div>{user.name}</div>
+                    <div>@{nameSplit[0]}1</div>
                   </span>
                 </div>
               </span>
             </li>
-
+            <hr />
             <li className="board-menu-nav-item">
               <span className="norm-item-content">
                 <div className="desc-icon" />
@@ -58,5 +79,8 @@ const MenuNavItemAbout = props => {
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  return { user: state.userReducer.user };
+};
 
-export default MenuNavItemAbout;
+export default connect(mapStateToProps)(MenuNavItemAbout);
