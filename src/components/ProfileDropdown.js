3630f73e262425d8731f992c88ac8df60334325e
William Sinn <w.h.sinn@gmail.com>
2020-08-01 13:40:10 -0400

    update profiledropdown with interface button with board, fix all styles

--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -2,24 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 import { setPage, userLogout } from "../actions/user";
 
-const ProfileDropdown = ({ user, closeRightnav, dispatch }) => {
+const ProfileDropdown = ({ user, closeRightnav, activePage, dispatch }) => {
   const renderCapsName = () => {
-    const names = user.name.split(" ");
-    let namesCaps;
-    if (names) {
-      names.forEach((name, index) => {
-        let capitalized = name[0].toUpperCase();
-        capitalized = capitalized + name.slice(1, name.length);
-        if (index === 0) {
-          namesCaps = capitalized;
-        }
-        if (index > 0) {
-          namesCaps = namesCaps + " " + capitalized;
-        }
-      });
+    if (user) {
+      const names = user.name.split(" ");
+      let namesCaps;
+      if (names) {
+        names.forEach((name, index) => {
+          let capitalized = name[0].toUpperCase();
+          capitalized = capitalized + name.slice(1, name.length);
+          if (index === 0) {
+            namesCaps = capitalized;
+          }
+          if (index > 0) {
+            namesCaps = namesCaps + " " + capitalized;
+          }
+        });
+      }
+      return namesCaps;
     }
-    return namesCaps;
   };
+
   return (
     <div className="new-board right-modal">
       <div className="dropdown-title ">
@@ -38,50 +41,74 @@ const ProfileDropdown = ({ user, closeRightnav, dispatch }) => {
           </button>
         </div>
       </div>
-      <ul>
-        <li className="option-item">
-          <button
-            onClick={(e) => dispatch(setPage("boards"), closeRightnav(e))}
-            className="option-title js-member-activity active"
-            data-tab="cards"
-          >
-            <span>Boards</span>
-          </button>
-        </li>
+      {activePage === "board" ? (
+        <ul>
+          <li className="option-item">
+            <a
+              href="/"
+              className="option-title js-member-account"
+              style={{ textDecoration: "none" }}
+            >
+              <span>Interface</span>
+            </a>
+          </li>
+          <li className="option-item">
+            <button
+              onClick={(e) => dispatch(userLogout(), closeRightnav(e))}
+              className="option-title js-member-account"
+              data-tab="settings"
+            >
+              <span>Logout</span>
+            </button>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li className="option-item">
+            <button
+              onClick={(e) => dispatch(setPage("boards"), closeRightnav(e))}
+              className="option-title js-member-activity active"
+              data-tab="cards"
+            >
+              <span>Boards</span>
+            </button>
+          </li>
 
-        <li className="option-item">
-          <button
-            onClick={(e) => dispatch(setPage("archives"), closeRightnav(e))}
-            className="option-title js-member-activity active"
-            data-tab="cards"
-          >
-            <span>Archives</span>
-          </button>
-        </li>
-        <li className="option-item">
-          <button
-            onClick={(e) => dispatch(setPage("settings"), closeRightnav(e))}
-            className="option-title js-member-activity active"
-            data-tab="cards"
-          >
-            <span>Settings</span>
-          </button>
-        </li>
-        <hr />
-        <li className="option-item">
-          <button
-            onClick={(e) => dispatch(userLogout(), closeRightnav(e))}
-            className="option-title js-member-account"
-            data-tab="settings"
-          >
-            <span>Logout</span>
-          </button>
-        </li>
-      </ul>
+          <li className="option-item">
+            <button
+              onClick={(e) => dispatch(setPage("archives"), closeRightnav(e))}
+              className="option-title js-member-activity active"
+              data-tab="cards"
+            >
+              <span>Archives</span>
+            </button>
+          </li>
+          <li className="option-item">
+            <button
+              onClick={(e) => dispatch(setPage("settings"), closeRightnav(e))}
+              className="option-title js-member-activity active"
+              data-tab="cards"
+            >
+              <span>Settings</span>
+            </button>
+          </li>
+          <hr />
+          <li className="option-item">
+            <button
+              onClick={(e) => dispatch(userLogout(), closeRightnav(e))}
+              className="option-title js-member-account"
+              data-tab="settings"
+            >
+              <span>Logout</span>
+            </button>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
 const mapStateToProps = (state) => ({
   user: state.userReducer.user,
+  activePage: state.userReducer.activePage,
 });
 export default connect(mapStateToProps)(ProfileDropdown);
