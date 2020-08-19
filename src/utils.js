bef2ca6c880f3f87804cbde551217237d6c0b596
William Sinn <w.h.sinn@gmail.com>
2020-08-18 22:07:39 -0400

    fix fetch link

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,6 @@ export const getCurrentDate = () => {
   return currDate[0];
 };
 
-export const fetchUrl = "localhost:3000";
+export const fetchUrl = "http://localhost:3000";
 
 //ask rob if theres a better way of exporting the fetch url.
